9a7b34dcc22deb300e76c07b414b3bfdd39881c1
salonibhingardive123 <saloni.bhingardive@coditas.com>
2026-06-22 00:44:31 +0530

    Update redis_client.py

--- backend/app/utils/redis_handle/redis_client.py
+++ backend/app/utils/redis_handle/redis_client.py
@@ -5,10 +5,7 @@ class RedisClient:
 
     @staticmethod
     def client():
-        return redis.Redis(
-            host=os.getenv("REDIS_HOST"),
-            port=int(os.getenv("REDIS_PORT", 6380)),
-            password=os.getenv("REDIS_PASSWORD"),
-            ssl=True,
+        return redis.from_url(
+            os.getenv("REDIS_URL"),
             decode_responses=True
         )
