49cd36abc70164f7ea050e853d3f1fa347a584b6
salonibhingardive123 <saloni.bhingardive@coditas.com>
2026-06-22 11:39:49 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -15,7 +15,7 @@ app.include_router(project_router)
 app.add_middleware(
        CORSMiddleware,
        allow_credentials=True,
-       allow_origins=["*"],
+       allow_origins=["https://lemon-sand-07a059e03.7.azurestaticapps.net"],
        allow_methods=["*"],
        allow_headers=["*"],
    )
